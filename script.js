40b24e1a1ca0b25954d7db95dfbae82ea6063096
Julian Marmier <julianmarmier@gmail.com>
2020-04-18 20:19:18 -0400

    Add Artist Statement

--- script.js
+++ script.js
@@ -25,6 +25,7 @@
 // })
 
 document.addEventListener('DOMContentLoaded', function(){
+      var currentObject = null;
       const sections = document.querySelectorAll(".section.ch");
       const menu_links = document.querySelectorAll("li");
 
@@ -48,26 +49,48 @@ document.addEventListener('DOMContentLoaded', function(){
       const images = document.querySelectorAll("img");
       const tooltip = document.getElementById('tooltip');
 
-      console.log(window.innerWidth)
+      const showTooltip = function (e, altText) {
+        currentObject = altText;
+        if (altText != "") {
+          tooltip.style.visibility = "visible";
+          tooltip.style.opacity = 1.0;
+          tooltip.innerHTML = `<p>${altText}</p>`
+          tooltip.style.top = e.pageY + 'px';
+          if ((e.pageX + tooltip.offsetWidth) < window.innerWidth) {
+            tooltip.style.left = e.pageX + 'px';
+          }
+        }
+      }
 
       for (var i = 0; i < images.length; i++) {
         const altText = images[i].alt;
-        images[i].addEventListener('mousemove', function (e) {
-          if (altText != "") {
-            tooltip.style.visibility = "visible";
-            tooltip.style.opacity = 1.0;
-            tooltip.innerHTML = `<p>${altText}</p>`
-            tooltip.style.top = e.pageY + 'px';
-            if ((e.pageX + tooltip.offsetWidth) < window.innerWidth) {
-              tooltip.style.left = e.pageX + 'px';
-            }
-          }
-        }, false)
+        images[i].addEventListener('mousemove', function(e) {showTooltip(e, altText)}, false);
         images[i].addEventListener('mouseout', function (e) {
             tooltip.style.visibility = "hidden";
             tooltip.style.opacity = 0;
+            currentObject = null;
         })
       }
+
+      const statement = document.querySelector("#statement");
+      statement.addEventListener('mousemove', function(e) {showTooltip(e, "Less is more")}, false);
+      statement.addEventListener('mouseout', function(e) {
+        tooltip.style.visibility = "hidden";
+        tooltip.style.opacity = 0;
+        currentObject = null;
+      })
+
+      document.addEventListener('wheel', function(e) {
+        if (currentObject != null) {
+          tooltip.style.visibility = "visible";
+          tooltip.style.opacity = 1.0;
+          tooltip.innerHTML = `<p>${currentObject}</p>`
+          tooltip.style.top = e.pageY + 'px';
+          if ((e.pageX + tooltip.offsetWidth) < window.innerWidth) {
+            tooltip.style.left = e.pageX + 'px';
+          }
+        }
+      }, false)
 }, false);
 
 function sliderChange(percent) {
